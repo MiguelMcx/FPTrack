32c40ea7715b0aa2c7489a35acb3e6d107878768
Miguel <miguelmaciasdelpino@gmail.com>
2026-05-15 18:10:29 +0200

    feat limpieza de backend para setup Postgresql

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express'
+import { PrismaClient } from "../generated/prisma/client";
+
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 
