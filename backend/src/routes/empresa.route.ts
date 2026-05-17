f1bba49e09978e42ec316bc82aac723ae90b6c50
Miguel <miguelmaciasdelpino@gmail.com>
2026-05-17 14:37:38 +0200

    feat crud empresas

--- backend/src/routes/empresa.route.ts
+++ backend/src/routes/empresa.route.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import { getEmpresas } from "../controllers/empresa.controller";
+import { postEmpresa } from "../controllers/empresa.controller";
 import { getById } from "../controllers/empresa.controller";
+import { deleteEmpresa } from "../controllers/empresa.controller";
+import { updateEmpresa } from "../controllers/empresa.controller";
 
 const router = Router();
 
 router.get('/',getEmpresas)
 router.get('/:id',getById)
-
+router.post('/', postEmpresa)
+router.delete('/:id', deleteEmpresa)
+router.put('/:id', updateEmpresa)
 
 export default router;
