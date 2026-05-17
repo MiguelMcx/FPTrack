7224e226ba756f8722f42daa66020c22da724d1e
Miguel <miguelmaciasdelpino@gmail.com>
2026-05-17 15:18:03 +0200

    feat Validaciones zod Empresa

--- backend/src/routes/empresa.route.ts
+++ backend/src/routes/empresa.route.ts
@@ -4,13 +4,15 @@ import { postEmpresa } from "../controllers/empresa.controller";
 import { getById } from "../controllers/empresa.controller";
 import { deleteEmpresa } from "../controllers/empresa.controller";
 import { updateEmpresa } from "../controllers/empresa.controller";
+import { validateEmpresa } from "../middlewares/empresa.middlewares";
+import { EmpresaSchema } from "../schema/empresa.schema";
 
 const router = Router();
 
 router.get('/',getEmpresas)
 router.get('/:id',getById)
-router.post('/', postEmpresa)
+router.post('/', validateEmpresa(EmpresaSchema), postEmpresa)
 router.delete('/:id', deleteEmpresa)
-router.put('/:id', updateEmpresa)
+router.put('/:id', validateEmpresa(EmpresaSchema), updateEmpresa)
 
 export default router;
